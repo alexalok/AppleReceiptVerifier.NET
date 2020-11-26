ead6efa92b42aca3456259e60c40e76236962489
alexalok <work@alexalok.me>
2020-11-26 19:13:32 +0300

    Resolver: cache resolved verifiers on a per-scope basis

--- AppleReceiptVerifier.NET/AppleReceiptVerifierResolver.cs
+++ AppleReceiptVerifier.NET/AppleReceiptVerifierResolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -7,7 +8,7 @@ namespace AppleReceiptVerifier.NET
 {
     public interface IAppleReceiptVerifierResolver
     {
-        AppleReceiptVerifier Resolve(string verifierName);
+        IAppleReceiptVerifier Resolve(string verifierName);
     }
 
     public class AppleReceiptVerifierResolver : IAppleReceiptVerifierResolver
@@ -15,23 +16,27 @@ namespace AppleReceiptVerifier.NET
         readonly IServiceProvider _services;
         readonly IHttpClientFactory _httpClientFactory;
 
+        readonly ConcurrentDictionary<string, IAppleReceiptVerifier> _createdVerifiers = new ConcurrentDictionary<string, IAppleReceiptVerifier>();
+
         public AppleReceiptVerifierResolver(IServiceProvider services)
         {
             _services = services;
             _httpClientFactory = _services.GetRequiredService<IHttpClientFactory>();
         }
 
-        public AppleReceiptVerifier Resolve(string verifierName)
+        public IAppleReceiptVerifier Resolve(string verifierName)
         {
             if (verifierName == AppleReceiptVerifierOptions.DefaultVerifierName)
                 throw new InvalidOperationException("Resolver can only be used when registering service implementations with non-default names.");
+            if (_createdVerifiers.TryGetValue(verifierName, out var verifier))
+                return verifier;
             verifierName = AppleReceiptVerifierOptions.ServicesPrefix + verifierName;
             var namedOptionsResolver = _services.GetRequiredService<IOptionsSnapshot<AppleReceiptVerifierOptions>>();
             var namedOptions = namedOptionsResolver.Get(verifierName);
             var options = Options.Create<AppleReceiptVerifierOptions>(namedOptions);
             var httpClient = _httpClientFactory.CreateClient(verifierName);
             var instance = ActivatorUtilities.CreateInstance<AppleReceiptVerifier>(_services, options, httpClient);
-            return instance;
+            return _createdVerifiers.GetOrAdd(verifierName, instance);
         }
     }
 }
